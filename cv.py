b17378ff59d2942d2b8e8455efa76b79520bc624
adviks-naik <66198321+adviks-naik@users.noreply.github.com>
2020-06-22 00:45:31 -0700

    Update cv.py

--- cv.py
+++ cv.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 
 b = np.ones([400,400],dtype='uint8')
-n = int(50)
 for i in range(n,400,n*2):
             for j in range (n,400,n*2):
              b[j-n:j,i-n:i] = 0
